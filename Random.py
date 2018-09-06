478027a3d62c86c9bb7eb9af04435b69466c68a0
aethercode <viniciusbyrne@gmail.com>
2018-09-05 21:54:48 -1000

    restructured the code and put generation into functions, also adding list feature

--- Random.py
+++ Random.py
@@ -1,17 +1,31 @@
+print("loading...")
+
 import random
 
-clock = 0
-verb = ("Tickles", "Eats", "Munches", "Punches", "Twists", "Shoots", "Beats", "Bends", "Bites", "Blows", "Breaks", "Builds", "Burns", "Catches", "Cuts", "Digs", "Dives", "Draws", "Dreams", "Feels", "Fights", "Freezes", "Grows", "Hangs", "Hears", "Hides", "Hurts", "Throws")
+verb = ("Tickles", "Eats", "Munches", "Punches", "Twists", "Shoots", "Beats", "Bends", "Bites", "Blows", "Breaks", "Builds", "Burns", "Catches", "Cuts", "Digs", "Dive", "Draws", "Dreams", "Feels", "Fights", "Freezes", "Grows", "Hangs", "Hears", "Hides", "Hurts", "Throws")
 noun = ("School", "Face", "Arm", "Mouth", "Head", "Nose", "Foot", "People", "History", "Art", "World", "Map", "Family", "Government", "Health", "System", "Computer", "Music", "Person", "Method", "Food", "Bird", "Literature", "Problem", "Software", "Knowledge", "Brain", "Economy", "Oven", "Friends")
 
+def generate():
+   return "That really "+random.choice(verb)+" my "+random.choice(noun)+"."
+def generator():
+   i = (input())
 
-while (clock==0):
+   if i=="generate":
+      print(generate())
+   elif i=="lmao":
+      print("I know right im so random")
 
-   e = (input())
+   if i!="bye":
+      generator()
+   else:
+      print("Bye!")
 
-   if e=="generate":
-      print("That really", random.choice(verb), "my", random.choice(noun), ".")
-   if e=="lmao":
-       print("I know right im so random")
-   if e=="bye":
-       clock = clock + 1
+
+def generateList(amt):
+   loop = 0
+   while loop <= amt:
+      print(str(loop)+": "+generate())
+      loop += 1
+
+print("done!")
+print("run generator() to start the generator, then type 'generate' to get a sentence. while in generate mode, type 'bye' to leave that mode. to generate a list exit generate mode if you're in it, then type generateList() with the amount you want inside the parentheses")
